b20a03aac773d65739049457d5cede0e887b7a2b
nikitaprokopenko <guardprokopenko@gmail.com>
2022-11-20 12:29:39 +0300

    Homework for lesson 3, added test for Search input

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,9 +1,14 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -33,5 +38,37 @@ public class FirstTest {
     @Test
     public void firstTest() {
         System.out.println("First test");
+    }
+
+    @Test
+    public void checkSearchHasText() {
+        assertElementHasText(
+                By.xpath("//*[@resource-id='org.wikipedia:id/search_container']//*[@class='android.widget.TextView']"),
+                "Search Wikipedia",
+                "Search doesn't contain expected text",
+                5
+        );
+    }
+
+    private WebElement waitForElementPresented(By by, String errorMessage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorMessage + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private void assertElementHasText(By by, String expectedText, String errorMessage, long timeoutInSecond) {
+        WebElement element = waitForElementPresented(
+                by,
+                "Element isn't presented",
+                timeoutInSecond
+        );
+        String elementText = element.getAttribute("text");
+        Assert.assertEquals(
+                errorMessage,
+                expectedText,
+                elementText
+        );
     }
 }
